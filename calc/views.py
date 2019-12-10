5767b3f3906783fa790e19d36fb66fa8920c4077
Shahzad Ahsan <dankara97@gmail.com>
2019-12-10 14:54:16 +0500

    make result.html and make simple cal to add two numbers

--- calc/views.py
+++ calc/views.py
@@ -4,7 +4,15 @@ from django.http import HttpResponse
 
 def home(request):
     #return HttpResponse('Hello Django')
-    return render(request , 'home.html' , {'name':'Shahzad Ahsan'}) #dlt jinga lang template 
+    return render(request , 'home.html' , {'name':'Shahzad Ahsan'}) #dlt jinja lang template 
+
+
+def  add(request):
+
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    res = val1 + val2
+    return render(request , 'result.html' , {'result':res})
 
 
 
