e5f01af6e3676f2461769ec347c42aa010a0f87e
Shahzad Ahsan <dankara97@gmail.com>
2019-12-11 15:46:26 +0500

    made index.html in templates and add path to urls of website

---
+++ travello/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+
+    path('' , views.index , name = 'index')
+    #path('', views.home , name = 'home'),
+    #path('add' , views.add , name = 'add')
+
+
+]
