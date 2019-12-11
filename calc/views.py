1d0262e1e9f1e0af3d5b9425409a6479118d09c0
Shahzad Ahsan <dankara97@gmail.com>
2019-12-11 13:16:59 +0500

    add POST method and resolve csrf error

--- calc/views.py
+++ calc/views.py
@@ -4,13 +4,15 @@ from django.http import HttpResponse
 
 def home(request):
     #return HttpResponse('Hello Django')
-    return render(request , 'home.html' , {'name':'Shahzad Ahsan'}) #dlt jinja lang template 
+    return render(request , 'home.html') #dlt jinja lang template 
 
 
-def  add(request):
+def add(request):
 
-    val1 = int(request.GET['num1'])
-    val2 = int(request.GET['num2'])
+    
+
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
     res = val1 + val2
     return render(request , 'result.html' , {'result':res})
 
