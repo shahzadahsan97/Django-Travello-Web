df99ff4dc606e6716c82a17ab0f5a7a38e5e2e76
Shahzad Ahsan <dankara97@gmail.com>
2019-12-18 16:40:43 +0500

    add postgresql to setting in database and connect model class to database

--- travello/models.py
+++ travello/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-class Destination():
-    id: int
-    price : int
-    desc : str
-    name : str
-    img : str
-    offer : bool
+class Destination(models.Model):
+    #id: int
+    price : models.IntegerField
+    desc : models.TextField
+    name : models.CharField(max_length=200)
+    img : models.ImageField(upload_to='pics')
+    offer : models.BooleanField(default = False)
 
 
 # Create your models here.
