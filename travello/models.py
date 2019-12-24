421523f4d7b28fced7d88f6b15f13ecc942c060e
Shahzad Ahsan <dankara97@gmail.com>
2019-12-24 14:33:55 +0500

    make migration of data

--- travello/models.py
+++ travello/models.py
@@ -2,11 +2,8 @@ from django.db import models
 
 class Destination(models.Model):
     #id: int
-    price : models.IntegerField
-    desc : models.TextField
-    name : models.CharField(max_length=200)
-    img : models.ImageField(upload_to='pics')
-    offer : models.BooleanField(default = False)
-
-
-# Create your models here.
+    price = models.IntegerField
+    desc = models.TextField
+    name = models.CharField(max_length=200)
+    img = models.ImageField(upload_to='pics')
+    offer = models.BooleanField(default = False)
