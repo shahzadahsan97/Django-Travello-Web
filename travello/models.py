9e3bc803f38feabe62181ffc8260ce2fd89df58d
Shahzad Ahsan <dankara97@gmail.com>
2019-12-31 13:26:26 +0500

    add price column to database

--- travello/models.py
+++ travello/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 class Destination(models.Model):
     #id: int
-    price = models.IntegerField
-    desc = models.TextField
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
     img = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    #info = models.TextField()
+    price = models.IntegerField()
     offer = models.BooleanField(default = False)
--- travello/views.py
+++ travello/views.py
@@ -10,6 +10,7 @@ def index(request):
     dest1.name = 'Karachi'
     dest1.desc = 'City of Lights'
     dest1.img = 'destination_1.jpg'
+    #dest1.info = 'Axiom Education'
     dest1.price = 900
     dest1.offer = False
 
@@ -17,6 +18,7 @@ def index(request):
     dest2.name = 'Lahore'
     dest2.desc = 'Eat Nehari'
     dest2.img = 'destination_2.jpg'
+    #dest2.info = 'Axiom Education'
     dest2.price = 700
     dest2.offer = False
 
@@ -24,6 +26,7 @@ def index(request):
     dest3.name = 'Islamabad'
     dest3.desc = 'Clean City , Captial'
     dest3.img = 'destination_3.jpg'
+    #dest3.info = 'Axiom Education'
     dest3.price = 1000
     dest3.offer = True
 
@@ -31,6 +34,7 @@ def index(request):
     dest4.name = 'Peshware'
     dest4.desc = 'Pista , Badaam'
     dest4.img = 'destination_4.jpg'
+    #dest4.info = 'Axiom Education'
     dest4.price = 150
     dest4.offer = True
 
@@ -38,6 +42,7 @@ def index(request):
     dest5.name = 'Quetta'
     dest5.desc = 'Damphook'
     dest5.img = 'destination_5.jpg'
+    #dest5.info = 'Axiom Education'
     dest5.price = 290
     dest5.offer = False
     
@@ -45,6 +50,7 @@ def index(request):
     dest6.name = 'Hyderabad'
     dest6.desc = 'Eat Karahi'
     dest6.img = 'destination_6.jpg'
+    #dest6.info = 'Axiom Education'
     dest6.price = 250
     dest6.offer = False
 
