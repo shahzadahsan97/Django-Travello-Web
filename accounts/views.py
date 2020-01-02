3ecd0c8159416e1b4ff9c847311c5c1c0df5eb92
Shahzad Ahsan <dankara97@gmail.com>
2020-01-02 17:57:55 +0500

    uisng POST method for the conditions

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def register(request):
-    return render(request , 'register.html')
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+
+    else:
+        return render(request , 'register.html')
