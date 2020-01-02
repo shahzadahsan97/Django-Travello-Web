018ba26476e758c991745e834b94463018b452be
Shahzad Ahsan <dankara97@gmail.com>
2020-01-02 17:28:44 +0500

    make new app and add accounts to index.html file to change design

--- website/urls.py
+++ website/urls.py
@@ -20,8 +20,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 
-    path('', include('travello.urls')),
+    path('', include('travello.urls')), #travello is the main module...
     path('admin/', admin.site.urls),
+    path('accounts/' , include('accounts.urls')),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
