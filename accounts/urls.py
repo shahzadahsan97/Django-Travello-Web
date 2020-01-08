36795ea0fd2f5dcf449effadce6a9b60ec67290c
Shahzad Ahsan <dankara97@gmail.com>
2020-01-08 14:23:13 +0500

    make login.html file and make login page using function and apply conditions on it

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
         path('register' , views.register , name = 'register'),
-
+        path('login' , views.login , name='login'),
 
 
 
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,24 @@ from django.contrib import messages
 from django.contrib.auth.models import User , auth
 
 # Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username , password=password)
+
+        if user is not None:
+            auth.login(request , user)
+            return redirect('/')
+        else:
+            messages.info(request , 'Sorry! Invalid Credentials.')
+            return redirect('login')
+    else:
+        return render(request , 'login.html')
+
+
 def register(request):
     if request.method == 'POST':
         first_name = request.POST['first_name']
@@ -12,16 +30,21 @@ def register(request):
         password2 = request.POST['password2']
         email = request.POST['email']
         if password1 == password2:
+
             if User.objects.filter(username=username).exists():
                 messages.info(request , 'User name is alreafy taken.\nPlease change your username.')
                 return redirect('register')
+
             elif User.objects.filter(email=email).exists():
                messages.info(request ,"Email is already in use.")
                return redirect('register')
+
             else:
                 user = User.objects.create_user(username=username , password=password1 , email=email , first_name=first_name , last_name=last_name)
                 user.save()
                 messages.info(request , "User is created.")
+                return redirect('login')
+
         else:
             messages.info(request ,"Password incorrct.\nPlease change your password and try again.")
             return redirect('register')
